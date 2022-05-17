e6495d29707d9c8fdf00dd756c4042cd96597d21
robin <rnj13@gmail.com>
2022-05-17 12:43:46 -0400

    send

--- main.cpp
+++ main.cpp
@@ -84,9 +84,9 @@ bool setup() {
 
 void send(const std::string& msg) {
     char write_buf[256]; //WARNING : The last character must be change line character!
-    ;
-    strcpy(write_buf, msg.c_str());
-    write(serial_port, write_buf, sizeof(write_buf));
+
+
+    write(serial_port, msg, sizeof(msg);
     std::cout << write_buf;
 }
 
